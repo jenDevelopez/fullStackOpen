fe37d7f62da3905f765884773a6af58bf548ba91
Jennifer López <jen1992lo@gmail.com>
2024-01-08 17:37:44 +0100

    Finished Part 1.2

---
+++ Part-1/src/components/Part1.jsx
@@ -0,0 +1,9 @@
+const Part1 = ({ part1, exercises1 }) => {
+  return (
+    <p>
+      {part1} {exercises1}
+    </p>
+  );
+};
+
+export default Part1;
---
+++ Part-1/src/components/Part3.jsx
@@ -0,0 +1,9 @@
+const Part3 = ({ part3, exercises3 }) => {
+  return (
+    <p>
+      {part3} {exercises3}
+    </p>
+  );
+};
+
+export default Part3;
