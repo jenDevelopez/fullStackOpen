f2341e4cb4e257a230d90f7eb544273a5e2d3e03
Jennifer López <jen1992lo@gmail.com>
2024-01-09 13:36:33 +0100

    Part 1.7 completed

--- Part-1/src/App.jsx
+++ Part-1/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import Button from "./components/Button";
+import Average from "./components/Average";
+import PositiveAverage from "./components/PositiveAverage";
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -8,6 +10,7 @@ const App = () => {
   const handleClickGood = () => setGood(good + 1);
   const handleClickNeutral = () => setNeutral(neutral + 1);
   const handleClickBad = () => setBad(bad + 1);
+  const all = good + neutral + bad
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -21,6 +24,11 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {all}</p>
+      </div>
+      <div>
+        <Average good={good} neutral={neutral} bad={bad} all={all} />
+        <PositiveAverage good={good} all={all}/>
       </div>
     </div>
   );
