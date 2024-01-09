43a54334df0ae24c46416bffe4f0e395560dd65c
Jennifer López <jen1992lo@gmail.com>
2024-01-09 13:47:36 +0100

    Part 1.9 completed

--- Part-1/src/components/Statistics.jsx
+++ Part-1/src/components/Statistics.jsx
@@ -1,17 +1,11 @@
-import Button from "./Button"
+
 import Average from "./Average"
 import PositiveAverage from "./PositiveAverage"
 PositiveAverage
-const Statistics = ({handleClickGood, handleClickNeutral, handleClickBad, good, neutral, bad, all}) => {
+const Statistics = ({good, neutral, bad, all}) => {
   return (
-    <div>
-      <h1>Give Feedback</h1>
-      <div>
-        <Button text="Good" handleClick={handleClickGood} />
-        <Button text="Neutral" handleClick={handleClickNeutral} />
-        <Button text="Bad" handleClick={handleClickBad} />
-      </div>
-      <h2>Statistics</h2>
+    <>
+      
       <div>
         <p>good {good}</p>
         <p>neutral {neutral}</p>
@@ -22,7 +16,7 @@ const Statistics = ({handleClickGood, handleClickNeutral, handleClickBad, good, 
         <Average good={good} neutral={neutral} bad={bad} all={all} />
         <PositiveAverage good={good} all={all}/>
       </div>
-    </div>
+    </>
   )
 }
 
