de5e859d77e08b4e8933df2e094fe09b6fbf39bb
Jennifer López <jen1992lo@gmail.com>
2024-01-10 14:55:58 +0100

    Part 2.3 and 2.4 completed

--- Part-2/course/src/components/Course.jsx
+++ Part-2/course/src/components/Course.jsx
@@ -4,8 +4,16 @@ import Header from "./Header"
 function Course({ course }) {
   return (
     <div>
-      <Header title={course.name} />
-      <Content parts={course.parts}/>
+      <ul>
+        {course.map((course) => (
+          <li key={course.id}>
+            <div>
+              <Header title={course.name} />
+              <Content parts={course.parts} />
+            </div>
+          </li>
+        ))}
+      </ul>
      </div>
   )
 }
