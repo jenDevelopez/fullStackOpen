ea39dedcafa014b93a76c65d5aa30d8e8dcf3dd8
Jennifer López <jen1992lo@gmail.com>
2024-01-10 15:53:57 +0100

    Part 2.6 completed

---
+++ Part-2b/phonebook/src/App.jsx
@@ -0,0 +1,47 @@
+import  { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addPerson = () => {
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    addPerson()
+    setNewName('')
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: 
+          <input 
+          onChange={(e) => setNewName(e.target.value)}
+          value={newName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>
+            {person.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
