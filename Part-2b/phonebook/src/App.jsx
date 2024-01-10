36cf4ab33049070f1a20f97eb8640c68e5f6431d
Jennifer López <jen1992lo@gmail.com>
2024-01-10 16:29:39 +0100

    Part 2.8 and 2.9 completed

--- Part-2b/phonebook/src/App.jsx
+++ Part-2b/phonebook/src/App.jsx
@@ -1,42 +1,65 @@
-import  { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
 
+  const [newName, setNewName] = useState("");
+  const [newPhoneNumber, setNewPhoneNumber] = useState("")
+  const [query, setQuery] = useState("")
   const addPerson = () => {
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newPhoneNumber
+    };
+
+    const personExist = persons.some(
+      (person) => person.name === newPerson.name
+    );
+
+    if (!personExist) {
+      setPersons(persons.concat(newPerson));
+    } else {
+      alert(`${newName} is already added to phonebook`);
     }
+  };
 
-    const personExist = persons.some(person => person.name === newPerson.name)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addPerson();
+    setNewName("");
+    setNewPhoneNumber("")
+  };
 
-    if(!personExist){
-      setPersons(persons.concat(newPerson))
-    }else {
-      alert(`${newName} is already added to phonebook`)
-    }
-  }
-
+  const filteredPersons = persons.filter((person) => person.name.toLowerCase().match(query.toLowerCase()))
   
-
-
-  const handleSubmit = e => {
-    e.preventDefault()
-    addPerson()
-    setNewName('')
-  }
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        filter shown with 
+        <input type="search"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}  />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: 
+          name:
           <input 
-          onChange={(e) => setNewName(e.target.value)}
-          value={newName}/>
+            onChange={(e) => setNewName(e.target.value)} 
+            value={newName} />
+         
+        </div>
+        <div>
+          number:
+          <input
+            onChange={(e) => setNewPhoneNumber(e.target.value)}
+            value={newPhoneNumber} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -44,14 +67,17 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
+        {filteredPersons.map((person) => (
           <li key={person.name}>
-            {person.name}
+            <p>
+              {person.name}
+              <span> {person.number}</span>
+            </p>
           </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
